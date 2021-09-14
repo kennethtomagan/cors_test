4414776bc4b6dac300473fe08a04edcff2e7f3cb
kennethtomaganfelix <kenneth.tomagan@felix.net>
2021-09-14 12:09:18 +0800

    Add form input

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <h1>CORS TEST</h1>
+    <h1 class="title">Felix CORS TEST</h1>
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" id="url" name="firstname" placeholder="Your" v-model="data.url">
+        <button type="submit"  @click="submit">Login</button>
+    </div>
   </div>
 </template>
 
@@ -8,29 +13,75 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import Vue from "vue";
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  },
-  mounted(){
+    name: 'App',
+    data() {
+        return {
+            data: {
+                url: null
+            }
+        };
+    },
+    methods: {
+        submit() {
         this.$axios
-        .get("https://local-searcher.pmstg.com/global/public_search/suburb?like=a")
-        .then(response => {
-            console.log(response);
-        }).catch(error=>{
-            console.log(error.response)
-        });
+            .get(this.data.url)
+            .then(response => {
+                console.log(response);
+            }).catch(error=>{
+                console.log(error.response)
+            });
+        }
     }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.title{
+    text-align: center;
+}
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+    padding: 16px;
+    width: 60%;
+    margin: auto;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    .container {
+        width: 100%;
+        margin: auto;
+    }
 }
 </style>
